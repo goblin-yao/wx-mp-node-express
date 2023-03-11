ea8d62e05e5c9f142fd6a0b41a22c5e4c23d2468
Junping Yao <junping.yao@seamoney.com>
2023-03-11 12:41:37 +0800

    添加路由

--- routes/gongzhonghao.js
+++ routes/gongzhonghao.js
@@ -10,7 +10,16 @@ router.post("/messages/send", async (req, res) => {
   // 从 header 中取appid，如果 from-appid 不存在，则不是资源复用场景，可以直接传空字符串，使用环境所属账号发起云调用
   const appid = req.headers["x-wx-from-appid"] || "";
   const { ToUserName, FromUserName, MsgType, Content, CreateTime } = req.body;
-  console.log("推送接收的账号", ToUserName, "创建时间", CreateTime);
+  console.log(
+    "推送接收的账号openid,appid",
+    openid,
+    "|",
+    appid,
+    "|",
+    ToUserName,
+    "创建时间",
+    CreateTime
+  );
   if (MsgType === "text") {
     const msgResult = await WXMsgChecker(openid, Content);
     let replyMsg = "";
